5a2a4541b0bae2ce001658768455948f886c3fe7
thomasvilches <thomas_vilches@hotmail.com>
2026-08-18 08:43:36 -0300

    teste2

--- codigo.R
+++ codigo.R
@@ -3,3 +3,17 @@ library(tidyverse)
 x <- c(10, 5, 6, 9, 15)
 
 plot(x)
+
+
+#### adicionando comentario
+
+a <- rnorm(10000)
+
+hist(a)
+
+
+#####################
+
+b  <- rnbinom(10000,20,0.5)
+hist(b)
+
